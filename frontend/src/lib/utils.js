c6e0ea25ed3eba34ed3a55d85b3226304b5bc24f
amolbhawari09-ops <amolbhawari09@gmail.com>
2026-03-02 13:42:48 +0530

    Create lib/utils.js

---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,20 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatNumber(num) {
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1) + 'M';
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + 'K';
+  }
+  return num.toString();
+}
+
+export function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
